1654db67def10b3fd9ff5893162a6916d267c152
titouane74 <titouane74@gmail.com>
2019-12-31 11:13:46 +0100

    FavoriFragment qui fonctionne pas à voir plus tard

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/FavoriFragment.java
@@ -0,0 +1,99 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.openclassrooms.entrevoisins.R;
+import com.openclassrooms.entrevoisins.di.DI;
+import com.openclassrooms.entrevoisins.model.Neighbour;
+import com.openclassrooms.entrevoisins.service.NeighbourApiService;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.List;
+
+
+public class FavoriFragment extends Fragment {
+
+    private static String TAG = "FAVORIFRAGMENT";
+    private NeighbourApiService mApiService;
+    private List<Neighbour> mNeighbours;
+    private RecyclerView mRecyclerView;
+
+    public FavoriFragment() {}
+
+    /**
+     * Create and return a new instance
+     * @return @{@link FavoriFragment}
+     */
+    public static FavoriFragment newInstance() {
+        FavoriFragment fragment = new FavoriFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mApiService = DI.getNeighbourApiService();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_favori_list, container, false);
+        Context context = view.getContext();
+        mRecyclerView = (RecyclerView) view;
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+
+        initListFavori();
+//        initList();
+        return view;
+    }
+
+    /**
+     * Init the List of neighbours Favorites
+     */
+    private void initListFavori() {
+        mNeighbours = mApiService.getNeighboursFavori();
+        mRecyclerView.setAdapter(new MyFavoriRecyclerViewAdapter(mNeighbours));
+    }
+    private void initList() {
+        mNeighbours = mApiService.getNeighbours();
+        mRecyclerView.setAdapter(new MyFavoriRecyclerViewAdapter(mNeighbours));
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    /**
+     * Fired if the user clicks on a delete button
+     * @param event
+     */
+/*    @Subscribe
+    public void onDeleteNeighbour(DeleteNeighbourEvent event) {
+        mApiService.deleteNeighbour(event.neighbour);
+        initListFavori();
+    }*/
+
+
+}
